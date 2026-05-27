4132aa7c8e694a25baf69e538ada07ade7005708
TrinidadSeg <trinidadasegovia@gmail.com>
2026-05-27 12:44:06 -0600

    New Comments

--- models.py
+++ models.py
@@ -7,11 +7,11 @@ class RequestContract(BaseModel):
     order_history_count: int
     e_ltv: float
     p_next_order_return_fraud: float = 0
-    p_next_order_return_not_fraud: float = 0.4
+    p_next_order_return_not_fraud: float = 0.4 # Based on Long-Time Data
     p_next_order_keep_fraud: float = 0
-    p_next_order_keep_not_fraud: float = 0.5
+    p_next_order_keep_not_fraud: float = 0.5 # Based on Long-Time Data
     p_next_order_deny_fraud: float = 0
-    p_next_order_deny_not_fraud: float = 0.1
+    p_next_order_deny_not_fraud: float = 0.1 # Based on Long-Time Data
 
 class ResponseContract(BaseModel):
     action: str
